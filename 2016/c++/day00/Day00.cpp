d239b5b2184917ecb5e7bd6ff300587d0b35587a
Fabian Würfl <fabian.wuerfl@gmx.at>
2017-04-15 09:48:13 +0200

    2016 c++ | Update template to include regex-stuff

--- 2016/c++/day00/Day00.cpp
+++ 2016/c++/day00/Day00.cpp
@@ -1,7 +1,18 @@
 #include "Day00.hpp"
 
+#include <regex>
+
+using namespace std;
+
 Day00::Day00(vector<string> input) : data(input) {
-    data = input;
+    for(string &line: input){
+        smatch sm;
+        regex reg(R"((.*))");
+        if( std::regex_match(line, sm, reg) ){
+            data.push_back(sm[1]);
+        }
+    }
+
 }
 
 Day00::~Day00(){
