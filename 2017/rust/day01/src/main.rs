7292aad7a03599c9e130e9422faf394fb5b07dcf
Fabian Würfl <fabian.wuerfl@gmx.at>
2017-12-03 08:27:06 +0100

    2017 Rust 01 | Fix input parsing.

--- 2017/rust/day01/src/main.rs
+++ 2017/rust/day01/src/main.rs
@@ -5,7 +5,7 @@ mod part2;
 fn main() {
     let day: i32 = 1;
 
-    let input = aocutils::import(day, Some("puzzle1"));
+    let input = aocutils::import(day, Some("puzzle1")).remove(0);
 
     let r1 = part1::solve(input.clone());
     let r2 = part2::solve(input);
