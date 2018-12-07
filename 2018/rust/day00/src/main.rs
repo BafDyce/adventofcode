0b90e12c1e8c6a9740568c9ac1a5c71a20157268
Fabian Würfl <bafdyce@tuta.io>
2018-12-07 19:18:16 +0100

    2018 Rust | Update template to make use of aoc_utils::PuzzleConfig

--- 2018/rust/day00/src/main.rs
+++ 2018/rust/day00/src/main.rs
@@ -62,18 +62,18 @@ fn main() {
         println!("Loading data from input file {}", input_name);
     }
     // READ & PARSE input
-    let input = parse_input(input_name, verbose);
+    let (input, puzzle_config) = parse_input(input_name, verbose);
 
     // SOLVE puzzles
-    let res1 = part1::solve(&input);
-    let res2 = part2::solve(&input);
+    let res1 = part1::solve(&input, &puzzle_config);
+    let res2 = part2::solve(&input, &puzzle_config);
 
     println!("results: {} and {}", res1, res2);
 }
 
-fn parse_input(input_name: &str, verbose: bool) -> InputType {
+fn parse_input(input_name: &str, verbose: bool) -> (InputType, PuzzleConfig) {
     let config = ImportConfig::new(2018, DAY, "../../_inputs/day{day}/");
-    let input = import(&config, input_name).unwrap();
+    let (input, puzzle_config) = import_with_puzzle_config(&config, input_name).unwrap();
     if verbose {
         println!("raw input: {:?}", input);
     }
@@ -82,7 +82,7 @@ fn parse_input(input_name: &str, verbose: bool) -> InputType {
     let data: InputType = input.into_iter().map(|line| {
         // Parsing logic
         // single numeric types
-        line.parse::<InputTypeSingle>().unwrap()
+        line.parse::<InputTypeSingle>().unwrap_or(0)
         ; // <-- REMOVE THIS IF NECESSARY!!
 
         // regex parsing stuff
@@ -103,6 +103,7 @@ fn parse_input(input_name: &str, verbose: bool) -> InputType {
 
     if verbose {
         println!("input parsed: {:?}", data);
+        println!("config: {:?}", puzzle_config);
     }
-    data
+    (data, puzzle_config)
 }
