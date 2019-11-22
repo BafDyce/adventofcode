6ebd3e0c0a4140736c5f409764b8db86b355e7ca
Fabian Würfl <bafdyce@tuta.io>
2019-11-22 18:03:17 +0100

    2019 00 Rust | Initial template

---
+++ 2019/rust/aoc_import_magic/src/lib.rs
@@ -0,0 +1,149 @@
+#[macro_use]
+extern crate serde_derive;
+
+use clap::{App, Arg};
+use serde_json::Value;
+use std::{
+    collections::HashMap,
+    fmt::Debug,
+    fs::OpenOptions,
+    io::{self, BufRead, Read},
+};
+
+// general stuff, should not be necessary to touch this
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct PuzzleOptions<T>
+where
+    T: Clone + Debug + PartialEq,
+{
+    pub day: i32,
+    pub input_name: String,
+    pub verbose: bool,
+    pub skip_p1: bool,
+    pub data: Option<T>,
+    pub config: HashMap<String, String>,
+}
+
+impl<T> PuzzleOptions<T>
+where
+    T: Clone + Debug + PartialEq,
+{
+    fn with_input_parsed<F>(mut self, parse_input_function: F) -> Result<Self, io::Error>
+    where
+        F: Fn(Vec<String>, &HashMap<String, String>, bool) -> T,
+    {
+        // first try as path
+        let input = read_input_file(&self.input_name).or(
+            // if that doesnt work (i.e. file not found), try to get it from _inputs dir
+            read_input_file(&format!(
+                "../../_inputs/day{:02}/{}.input",
+                self.day, self.input_name
+            )),
+        )?;
+
+        if self.verbose {
+            println!("raw input: {:?}", input);
+        }
+
+        // dont fail if we
+        match read_config_file(&format!(
+            "../../_inputs/day{:02}/{}.config",
+            self.day, self.input_name
+        )) {
+            Ok(config) => {
+                if self.verbose {
+                    println!("config (file): {:#?}", config);
+                }
+                for (kk, vv) in config.into_iter() {
+                    if !self.config.contains_key(&kk) {
+                        self.config.insert(kk, {
+                            match vv {
+                                Value::String(vv) => vv.to_owned(),
+                                Value::Bool(vv) => format!("{}", vv),
+                                Value::Number(vv) => format!("{}", vv),
+                                other => panic!(format!("Value type not supported ({:?})", other)),
+                            }
+                        });
+                    }
+                }
+            }
+            Err(ee) => {
+                println!(
+                    "[WARN] Failed to load config file. Will continue either way: {}",
+                    ee
+                );
+            }
+        }
+
+        self.data = Some(parse_input_function(input, &self.config, self.verbose));
+        if self.verbose {
+            println!("parsed input: {:#?}", self.data.as_ref().unwrap());
+        }
+
+        Ok(self)
+    }
+}
+
+fn read_config_file(path: &str) -> Result<HashMap<String, Value>, io::Error> {
+    let file = OpenOptions::new().read(true).write(false).create(false).open(path)?;
+    let mut contents = String::new();
+    io::BufReader::new(file).read_to_string(&mut contents)?;
+
+    Ok(serde_json::from_str::<HashMap<String, Value>>(&contents).unwrap())
+}
+
+fn read_input_file(path: &str) -> Result<Vec<String>, io::Error> {
+    let file = OpenOptions::new().read(true).write(false).create(false).open(path)?;
+    io::BufReader::new(file).lines().collect()
+}
+
+pub fn import_magic<T, F>(day: i32, parse_input_function: F) -> Result<PuzzleOptions<T>, io::Error>
+where
+    T: Debug + Clone + PartialEq,
+    F: Fn(Vec<String>, &HashMap<String, String>, bool) -> T,
+{
+    let args = App::new(format!("aoc-2019-{:02}", day))
+        .author("Fabian Würfl <bafdyce@tuta.io>")
+        .arg(Arg::with_name("input")
+            .long("input")
+            .help("name of the config file")
+            .takes_value(true)
+            .default_value("puzzle1")
+        )
+        .arg(Arg::with_name("skip_p1")
+            .long("skip--first-part")
+            .help("Skip first part and directly compute second")
+        )
+        .arg(Arg::with_name("verbose")
+            .short("v")
+            .long("verbose")
+            .help("verbose output")
+        )
+        .arg(Arg::with_name("config")
+            .short("c")
+            .long("config")
+            .help("Specify key value pair for puzzle configuration. This overrides values defined in the input file config")
+            .number_of_values(2)
+            .multiple(true)
+            .value_delimiter("=")
+        )
+        .get_matches();
+
+    PuzzleOptions {
+        day,
+        input_name: args.value_of("input").unwrap().to_owned(),
+        verbose: args.is_present("verbose"),
+        skip_p1: args.is_present("skip_p1"),
+        data: None,
+        config: match args.values_of("config") {
+            Some(config_options) => config_options
+                .collect::<Vec<&str>>()
+                .as_slice()
+                .chunks(2)
+                .map(|list| (list[0].to_owned(), list[1].to_owned()))
+                .collect(),
+            _ => HashMap::new(),
+        },
+    }
+    .with_input_parsed(parse_input_function)
+}
---
+++ 2019/rust/day00/src/main.rs
@@ -0,0 +1,98 @@
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate serde_derive;
+
+use aoc_import_magic::{import_magic, PuzzleOptions};
+use regex::Regex;
+use std::{
+    collections::{HashMap, VecDeque},
+    io,
+};
+
+const DAY: i32 = 0;
+type InputTypeSingle = usize;
+type InputType = Vec<InputTypeSingle>;
+type OutputType1 = usize;
+type OutputType2 = OutputType1;
+type TodaysPuzzleOptions = PuzzleOptions<InputType>;
+
+#[derive(Clone, Copy, Debug, Default, Deserialize, PartialEq, Serialize)]
+struct Data {
+
+}
+
+impl Data {
+    pub fn new() -> Self {
+        Data {
+
+        }
+    }
+}
+
+impl From<()> for Data {
+    fn from(from: ()) -> Data {
+        Data {
+
+        }
+    }
+}
+
+fn main() -> Result<(), io::Error> {
+    println!("AoC 2019 | Day {}", DAY);
+
+    // This function is pure magic (see ../../aoc_import_magic/lib.rs) because it
+    // 1. parses command line arguments
+    // 2. reads the input file for the correct day
+    // 3. uses `parse_input` as a parsing function
+    // 4. returns a nice usable struct which contains everything which we need for the actual puzzle
+    let puzzle = import_magic(DAY, parse_input)?;
+    let res1 = if puzzle.skip_p1 {
+        None
+    } else {
+        let res1 = part1(&puzzle);
+        println!("Part 1 result: {}", res1);
+        Some(res1)
+    };
+
+    let res2 = part2(&puzzle, res1);
+    println!("Part 2 result: {}", res2);
+
+    Ok(())
+}
+
+fn parse_input(input: Vec<String>, config: &HashMap<String, String>, verbose: bool) -> InputType {
+    // PARSE input
+    input
+        .into_iter()
+        .map(|line| {
+            // Parsing logic
+            // single numeric types
+
+            line.parse::<InputTypeSingle>().unwrap_or_default()
+            ; // <-- REMOVE THIS IF NECESSARY!!
+
+            // regex parsing stuff
+            lazy_static! {
+                // (?x)
+                // (?P<name>xxx)
+                static ref RE: Regex = Regex::new(
+                    r"([[:alpha:]])*"
+                ).unwrap();
+            }
+
+            let caps = RE.captures(&line).unwrap();
+            // let thingy = &caps["thingy"];
+            // let xx = caps["xx"].chars().next().unwrap();
+            caps.len()
+        })
+        .collect()
+}
+
+fn part1(po: &TodaysPuzzleOptions) -> OutputType1 {
+    OutputType1::default()
+}
+
+fn part2(po: &TodaysPuzzleOptions, res1: Option<OutputType1>) -> OutputType2 {
+    OutputType2::default()
+}
